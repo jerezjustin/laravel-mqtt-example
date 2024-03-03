56e41f24809cc48e9ebab16c28c204d99a5c2e04
Justin Jerez <justinjerez@hotmail.com>
2024-03-03 19:54:07 -0400

    Install & configure websockets, mqtt, pusher and echo packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,10 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,6 +16,16 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        window.Echo = new Echo({
+            broadcaster: 'pusher',
+            key: import.meta.env.VITE_PUSHER_APP_KEY,
+            cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+            wsHost: import.meta.env.VITE_PUSHER_HOST,
+            wsPort: import.meta.env.VITE_PUSHER_PORT,
+            forceTLS: (import.meta.env.VITE_PUSHER_SCHEME === 'https'),
+            disableStats: true,
+        });
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
